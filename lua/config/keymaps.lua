9971b2d3c8eb31363c889ae00db035e4f2db4c0c
Jase Payne <jase.payne@gmail.com>
2025-11-12 15:16:30 -0600

    Folds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,22 +7,19 @@ vim.keymap.set('i', '<M-S-Down>', '<C-o>:move +1<cr>', { desc = 'Move Line Down'
 -- Quickly source current file / execute Lua code
 vim.keymap.set('n', '<leader>xx', '<Cmd>source %<CR>', { desc = 'Source current file' })
 
--- Ctrl-L redraws the screen by default. Now it will also toggle search highlighting.
+-- Toggle search highlighting.
 vim.keymap.set('n', '<C-l>', ':set hlsearch!<cr><C-l>', { desc = 'Toggle search highlighting' })
 
 -- Diagnostic keymaps
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- nvim-tree keymaps
--- vim.keymap.set('n', '<F6>', ':NvimTreeToggle<CR>', { desc = '[T]oggle Nvim-Tree' })
-
 -- Fyler keymaps
 vim.keymap.set('n', '<F6>', ':Fyler<CR>', { desc = 'Launch Fyler' })
 vim.keymap.set('v', '<F6>', ':Fyler<CR>', { desc = 'Launch Fyler' })
 vim.keymap.set('n', '<leader><F6>', ':Fyler<CR>', { desc = 'Launch Fyler' })
 
--- nvim-Floaterm keymaps
+-- Floaterm keymaps
 vim.keymap.set('n', '<F7>', ':FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' })
 vim.keymap.set('t', '<F7>', '<C-n>:FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' })
 
