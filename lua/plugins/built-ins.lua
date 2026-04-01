8ad32d18c052d786c6058e0163735fdbb77b75b8
Jase Payne <jase.payne@gmail.com>
2026-04-01 15:26:50 -0500

    Format the undotree window

--- lua/plugins/built-ins.lua
+++ lua/plugins/built-ins.lua
@@ -1,6 +1,13 @@
+-- A catchall to enable some built-ins that came with .12
+
 -- undotree
 vim.cmd("packadd nvim.undotree")
-vim.keymap.set("n", "<leader>u", require("undotree").open)
+vim.keymap.set("n", "<leader>u", function()
+	require("undotree").open({
+		command = math.floor(vim.api.nvim_win_get_width(0) / 3) .. "vnew",
+	})
+end, { desc = "[U]ndotree toggle" })
+
 
 -- clear search highlight on insert
 vim.cmd("packadd nohlsearch")
