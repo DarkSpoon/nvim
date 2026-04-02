ace5d5c1692951240ae6613380127468bf50fbae
Jase Payne <jase.payne@gmail.com>
2026-04-02 11:00:46 -0500

    Cleanup

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -3,17 +3,19 @@ vim.pack.add({
     -- "https://github.com/nvim-telescope/telescope.nvim"
 });
 
--- local builtin = require("telescope.builtin")
 local osname = vim.loop.os_uname().sysname
 local starter = require("mini.starter")
+local builtin = require("telescope.builtin")
 local pad = string.rep(" ", 22)
 local confDir = ""
 
+-- Dumb hack. In Windows, when a file is picked from the list after the CWD is set by anything except a hardcoded string, the C: drive changes to U:
 if osname == 'Windows_NT' then
     confDir = "C:/Users/jpayne/AppData/Local/nvim/"
 else
     confDir = vim.fn.stdpath 'config'
 end
+
 local new_section = function(name, action, section)
     return { name = name, action = action, section = pad .. section }
 end
@@ -31,9 +33,9 @@ require('mini.starter').setup({
         new_section("files",        "Telescope find_files",                 "Find"),
         new_section("recent ",     "Telescope oldfiles",                    "Find"),
         new_section("text",        "Telescope live_grep",                   "Find"),
-        -- new_section("config files",          function() require("telescope.builtin").find_files { cwd = vim.fn.stdpath 'config' } end,     "Config"),
-        -- new_section("config files",          function() require("telescope.builtin").find_files { cwd = os.getenv("NVIMCONFIG") } end,     "Config"),
-        new_section("config files",          function() require("telescope.builtin").find_files { cwd = confDir} end,     "Config"),
+        -- new_section("config files",          function() builin.find_files { cwd = vim.fn.stdpath 'config' } end,     "Config"),
+        -- new_section("config files",          function() builtin.find_files { cwd = os.getenv("NVIMCONFIG") } end,     "Config"),
+        new_section("config files",          function() builtin.find_files { cwd = confDir} end,     "Config"),
         new_section("update",            ":lua vim.pack.update()",      "Config"),
         new_section("new file",        "ene | startinsert",                 "System"),
         new_section("explore",          "Yazi cwd",                         "System"),
