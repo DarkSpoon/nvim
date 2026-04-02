ef18b85380d0613415e0a6a53db5f98593e6d549
Jase Payne <jase.payne@gmail.com>
2026-04-02 12:28:57 -0500

    Added current date time footer

--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -19,6 +19,23 @@ end
 local new_section = function(name, action, section)
     return { name = name, action = action, section = pad .. section }
 end
+
+local DateTime = (function()
+    local timer = vim.loop.new_timer()
+    -- timer:start(0, 1000, vim.schedule_wrap(function()
+    timer:start(0, 5000, vim.schedule_wrap(function()
+        if vim.bo.filetype ~= 'ministarter' then
+            timer:stop()
+            return
+        end
+        MiniStarter.refresh()
+    end))
+
+    return function()
+        return pad .. os.date("%x") .. "  " .. os.date("%I") .. ":" .. os.date("%M") .. " " .. os.date("%p")
+    end
+end)()
+
 require('mini.starter').setup({
     header = table.concat({
     "       ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗       ",
@@ -41,6 +58,7 @@ require('mini.starter').setup({
         new_section("explore",          "Yazi cwd",                         "System"),
         new_section("quit",            "qa",                                "System"),
     },
+    footer = DateTime,
     content_hooks = {
         starter.gen_hook.adding_bullet(pad .. "░ ", false),
         starter.gen_hook.aligning("center", "center"),
