3da73a0afb80783167f80bae186cbacb3dc4e93b
Jase Payne <jase.payne@gmail.com>
2026-04-06 14:53:31 -0500

    treesitter updates

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,10 +6,50 @@
 --
 -- }
 
+-- vim.pack.add({
+--     {
+--         src = "https://github.com/nvim-treesitter/nvim-treesitter",
+--         branch = "master",
+--         build = ':TSUpdate'
+--     }
+-- })
 vim.pack.add({
     {
         src = "https://github.com/nvim-treesitter/nvim-treesitter",
-        branch = "master",
-        build = ':TSUpdate'
+        version = "main",
+        -- build = ':TSUpdate'
     }
+})
+
+require("nvim-treesitter").setup({})
+require("nvim-treesitter").install({
+    "arduino",
+    "bash",
+    "gitcommit",
+    "gitignore",
+    "ini",
+    "json", "jsonc",
+    "lua",
+    "make",
+    "markdown", "markdown_inline",
+    "powershell",
+    "python",
+    "toml",
+    "vim",
+    "yaml"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "*" } ,
+    callback = function()
+        local filetype = vim.bo.filetype
+        if filetype and filetype ~= "" then
+            local success = pcall(function()
+                vim.treesitter.start()
+            end)
+            if not success then
+                return
+            end
+        end
+    end,
 })
