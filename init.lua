67aea1755636e676b7eb77d26ef4d7340dd4cbb4
Jase Payne <jase.payne@gmail.com>
2026-04-07 18:24:14 -0500

    Corrected loading order

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("plugins")
-require("config.colorscheme")
 require("config.globals")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("plugins")
+require("config.colorscheme")
