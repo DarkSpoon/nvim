4c5b0bc33a850fdb2f41f58fcb0eb957934c32e0
Jase Payne <jase.payne@gmail.com>
2026-04-07 15:24:43 -0500

    Cleanup

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,11 +1,1 @@
--- return {
---     "ellisonleao/gruvbox.nvim",
---     priority = 1000 ,
---     lazy = true,
---     config = true,
---     opts = {
---           contrast = "light"
---     }
--- }
-
 vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim" })
