dbce73b1513d4d5296d4f8accfa4976385c2708f
Jase Payne <jase.payne@gmail.com>
2026-05-01 15:24:20 -0500

    Change cwd on close true

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,6 +1,6 @@
 require('yazi').setup{
     -- open_for_directories = true,
-    -- change_neovim_cwd_on_close = true,
+    change_neovim_cwd_on_close = true,
     integrations = {
         grep_in_directory = "snacks.picker",
         grep_in_selected_files = "snacks.picker"
