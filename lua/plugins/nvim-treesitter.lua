edd038f6fb54445a13157b3dd0dd9c61a366bef5
Jase Payne <jase.payne@gmail.com>
2026-05-04 14:56:05 -0500

    Trigger treesitter via autocmd for each filetype we install parsers for

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -23,17 +23,17 @@ require("nvim-treesitter").install({
     "yaml"
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "*" } ,
-    callback = function()
-        local filetype = vim.bo.filetype
-        if filetype and filetype ~= "" then
-            local success = pcall(function()
-                vim.treesitter.start()
-            end)
-            if not success then
-                return
-            end
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = { "*" } ,
+--     callback = function()
+--         local filetype = vim.bo.filetype
+--         if filetype and filetype ~= "" then
+--             local success = pcall(function()
+--                 vim.treesitter.start()
+--             end)
+--             if not success then
+--                 return
+--             end
+--         end
+--     end,
+-- })
