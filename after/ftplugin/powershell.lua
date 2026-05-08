9e0129c8008caeb005a4e1af710f1bf56ce5c5aa
Jase Payne <jase.payne@gmail.com>
2026-05-08 11:33:52 -0500

    Change ps1.lua to powershell.lua to get it to load with the new 'powershell' filetype

--- after/ftplugin/powershell.lua
+++ after/ftplugin/powershell.lua
@@ -1,3 +1,4 @@
+-- This needs the same name as the filetype. If the Global is not set 'ps1' will be the filetype
 require("which-key").add({{ "<leader>p", group = "powershell" },})
 
 vim.keymap.set({ 'n','v' }, '<Leader>pb', '<esc>i<#<cr>.SYNOPSIS<cr>.DESCRIPTION<cr>.PARAMETER $Param<cr>.INPUTS<cr>.OUTPUTS<cr>.EXAMPLE<cr>#><cr><esc>', { desc = 'Generate Powershell Usage', buffer = true } )
