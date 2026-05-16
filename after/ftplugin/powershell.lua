786792686ba90405e9265ad6f823a15843c9f483
Jase Payne <jase.payne@gmail.com>
2026-05-16 16:33:59 -0500

    Clarified notes

--- after/ftplugin/powershell.lua
+++ after/ftplugin/powershell.lua
@@ -1,4 +1,4 @@
--- This needs the same name as the filetype. If the Global is not set 'ps1' will be the filetype
+-- This should be named the same as the filetype. If the Global is not set 'ps1' will be the filetype and this should be named ps1.lua
 require("which-key").add({{ "<leader>p", group = "powershell" },})
 
 vim.keymap.set({ 'n','v' }, '<Leader>pb', '<esc>i<#<cr>.SYNOPSIS<cr>.DESCRIPTION<cr>.PARAMETER $Param<cr>.INPUTS<cr>.OUTPUTS<cr>.EXAMPLE<cr>#><cr><esc>', { desc = 'Generate Powershell Usage', buffer = true } )
